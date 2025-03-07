a87b04a919c4f42af33b5c2c34633d6e5e8e9b7d
Jian Godito <jgodito27@gmail.com>
2025-03-07 08:53:56 +0800

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,33 +1,5 @@
-import axios from "axios";
+export default funtion HomePage() {
 
-const API_BASE_URL = "https://reqres.in/api";
+return <div> HomePage </div> 
 
-const api = axios.create({
-  baseURL: API_BASE_URL,
-  headers: { "Content-Type": "application/json" },
-});
-
-export const fetchUsers = async () => {
-  const response = await api.get("/users?page=1");
-  return response.data.data;
-};
-
-export const fetchUserById = async (id: number) => {
-  const response = await api.get(`/users/${id}`);
-  return response.data.data;
-};
-
-export const createUser = async (name: string, job: string) => {
-  const response = await api.post("/users", { name, job });
-  return response.data;
-};
-
-export const updateUser = async (id: number, name: string, job: string) => {
-  const response = await api.put(`/users/${id}`, { name, job });
-  return response.data;
-};
-
-export const deleteUser = async (id: number) => {
-  await api.delete(`/users/${id}`);
-  return { message: "User deleted successfully" };
-};
+}
